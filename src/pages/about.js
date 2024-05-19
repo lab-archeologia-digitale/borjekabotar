0fa9cb263a104738e25631745450468fdc7f27b7
Julian Bogdani <jbogdani@gmail.com>
2024-05-19 21:52:57 +0200

    Updated Team

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import { StaticImage } from "gatsby-plugin-image";
 import { GatsbySeo } from "gatsby-plugin-next-seo";
 
 import Layout from "../templates/ConLayout";
-import { Row, Col, Card, CardGroup } from "react-bootstrap";
+import { Row, Col, Card, Container } from "react-bootstrap";
 
 const team = () => {
   return (
@@ -24,34 +24,99 @@ const team = () => {
           />
           <h2>Team</h2>
         </div>
-        <Row className="col-md-8 mx-auto my-5">
-          <Col>
-            <CardGroup>
+
+        <Container>
+          <Row className="my-5">
+            <Col>
               <Card>
                 <Card.Body>
                   <Card.Title>Danilo Rosati</Card.Title>
                   <Card.Text className="position">
-                    Principal Investigator
+                    Project Co-Director
                   </Card.Text>
                 </Card.Body>
               </Card>
+            </Col>
+            <Col>
               <Card>
                 <Card.Body>
                   <Card.Title>Fariba Saiedi Anaraki</Card.Title>
                   <Card.Text className="position">
-                    Principal Investigator
+                    Project Co-Director
                   </Card.Text>
                 </Card.Body>
               </Card>
+            </Col>
+          </Row>
+          <Row className="my-5">
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Francesca Adesso</Card.Title>
+                  <Card.Text className="position">3D Modeler</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Domizia D'Erasmo</Card.Title>
+                  <Card.Text className="position">Web-GIS Developer</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Edoardo Ferrari</Card.Title>
+                  <Card.Text className="position">Architect</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Noemi Giovino</Card.Title>
+                  <Card.Text className="position">Web Developer</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row className="my-5">
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Amir Jamshidi</Card.Title>
+                  <Card.Text className="position">Translator</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Gaetano Pezzella</Card.Title>
+                  <Card.Text className="position">Photographer</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Giuseppe Salemmi</Card.Title>
+                  <Card.Text className="position">3D Survey</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
               <Card>
                 <Card.Body>
                   <Card.Title>Ilaria Elisea Scerrato</Card.Title>
-                  <Card.Text className="position">Anthropologist</Card.Text>
+                  <Card.Text className="position">Social Anthropologist</Card.Text>
                 </Card.Body>
               </Card>
-            </CardGroup>
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+        </Container>
       </Layout>
     </div>
   );
